928b694b8c33ad942d00a0eca297abc48ea34075
Dang Hoang Ha <ha.dh290702@gmail.com>
2024-05-02 23:20:28 +0300

    About section + Skills

--- pages/about/index.js
+++ pages/about/index.js
@@ -15,6 +15,11 @@ import {
         SiFirebase,
 } from 'react-icons/si';
 
+import Avatar from '../../components/Avatar';
+import Circles from '../../components/Circles';
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+import { fadeIn } from '../../variants';
 //  data
 const aboutData = [
         {
@@ -39,11 +44,11 @@ const aboutData = [
                         },
                         {
                                 title: 'Mobile Development',
-                                icon: [<SiKotlin />, <FaReact />],
+                                icons: [<SiKotlin />, <FaReact />],
                         },
                         {
                                 title: 'Version Control && Database',
-                                icon: [
+                                icons: [
                                         <FaGithub />,
                                         <FaGitlab />,
                                         <SiMysql />,
@@ -98,7 +103,69 @@ const aboutData = [
 ];
 
 const About = () => {
-        return <div>About</div>;
+        const [index, setIndex] = useState(0);
+        console.log(index);
+        return (
+                <div className="h-full bg-primary/30 py-48 text-center xl:text-left">
+                        <Circles></Circles>
+                        {/* Avatar */}
+                        <motion.div
+                                variants={fadeIn('right', 0.2)}
+                                initial="hidden"
+                                animate="show"
+                                exit="hidden"
+                                className="hidden xl:flex absolute bottom-0 -left-[360px]"
+                        >
+                                <Avatar />
+                        </motion.div>
+                        <div className="container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6">
+                                <div className="flex-1 flex flex-col justify-center">Hello</div>
+                                <div className="flex flex-col w-full xl:max-w-[48%] h-[480px]">
+                                        <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
+                                                {aboutData.map((item, itemIndex) => {
+                                                        return (
+                                                                <div
+                                                                        key={itemIndex}
+                                                                        onClick={() => setIndex(itemIndex)}
+                                                                        className={`${
+                                                                                index === itemIndex &&
+                                                                                'text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-all'
+                                                                        } cursor-pointer capitalize xl:text-xl text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0`}
+                                                                >
+                                                                        {item.title}
+                                                                </div>
+                                                        );
+                                                })}
+                                        </div>
+                                        <div className="py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start">
+                                                {aboutData[index].info.map((item, itemIndex) => {
+                                                        return (
+                                                                <div
+                                                                        key={itemIndex}
+                                                                        className="flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center"
+                                                                >
+                                                                        <div className="font-light mb-2 md:mb-0">
+                                                                                {item.title}
+                                                                        </div>
+                                                                        <div className="hidden md:flex">-</div>
+                                                                        <div>{item.date}</div>
+                                                                        <div className="flex gap-x-4 mb-4 md:mb-0">
+                                                                                {item.icons?.map((icon, itemIndex) => {
+                                                                                        return (
+                                                                                                <div className="text-2xl transition-all duration-500">
+                                                                                                        {icon}
+                                                                                                </div>
+                                                                                        );
+                                                                                })}
+                                                                        </div>
+                                                                </div>
+                                                        );
+                                                })}
+                                        </div>
+                                </div>
+                        </div>
+                </div>
+        );
 };
 
 export default About;
